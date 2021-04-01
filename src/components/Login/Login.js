0a26bd3d884bf3398835f906a49bb688cbc29a8e
nhan <dinhnhan3007@gmail.com>
2021-04-01 11:48:39 +0700

    asd

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -31,52 +31,50 @@ export default function Login({setToken}) {
   };
 
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="card o-hidden border-0 shadow-lg my-5 w-50">
-          <div className="card-body p-0">
-            <div className="justify-content-center row ">
-              <div className="col-lg-6">
-                <div>
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                  <div className="text-center">
-                    <h1 className="h3 text-gray-900 mb-4">Admin</h1>
-                  </div>
-                  <form className="user" onSubmit={handleSubmit}>
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        className="form-control form-control-user"
-                        id="exampleInputEmail"
-                        aria-describedby="emailHelp"
-                        placeholder="Email "
-                        onChange={(e) => setEmail(e.target.value)}
-                      />
-                    </div>
-                    <div className="form-group">
-                      <input
-                        type="password"
-                        className="form-control form-control-user"
-                        id="exampleInputPassword"
-                        placeholder="Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                      />
-                    </div>
-                    <button
-                      type="submit"
-                      className="btn btn-primary btn-user btn-block"
-                    >
-                      Login
-                    </button>
-                  </form>
-                  <br />
-                  <br />
-                  <br />
-                  <br />
+    <div className="container d-flex justify-content-center my-5 align-items-center ">
+      <div className="card o-hidden border-0 shadow-lg my-5 w-50">
+        <div className="card-body p-0">
+          <div className="justify-content-center row ">
+            <div className="col-lg-6">
+              <div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <div className="text-center">
+                  <h1 className="h3 text-gray-900 mb-4">Admin</h1>
                 </div>
+                <form className="user" onSubmit={handleSubmit}>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control form-control-user"
+                      id="exampleInputEmail"
+                      aria-describedby="emailHelp"
+                      placeholder="Email "
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className="form-control form-control-user"
+                      id="exampleInputPassword"
+                      placeholder="Password"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-user btn-block"
+                  >
+                    Login
+                  </button>
+                </form>
+                <br />
+                <br />
+                <br />
+                <br />
               </div>
             </div>
           </div>
