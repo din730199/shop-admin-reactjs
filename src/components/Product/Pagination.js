50c3a31a698393ed27d049b904981f90ae04ec40
nhan <dinhnhan3007@gmail.com>
2021-04-03 16:04:06 +0700

    pagination center

--- src/components/Product/Pagination.js
+++ src/components/Product/Pagination.js
@@ -9,7 +9,7 @@ const Pagination = ({postsPerPage, totalPosts, paginate}) => {
 
   return (
     <nav>
-      <ul className="pagination">
+      <ul className="pagination justify-content-center">
         {pageNumbers.map((number) => (
           <li key={number} className="page-item">
             <button
